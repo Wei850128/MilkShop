1b051b2158b4dce9bad9d2c0452b51710ed7c58e
Wei <a0983161756@gmail.com>
2020-09-30 15:36:58 +0800

    aaa

--- Milkshop/Cartlist/CartTableViewCell.swift
+++ Milkshop/Cartlist/CartTableViewCell.swift
@@ -23,7 +23,7 @@ class CartTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
